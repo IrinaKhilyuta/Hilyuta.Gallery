3db9f2555163bf4dadabc6c214943e6874803321
xBIGMAN <ira_hill@mail.ru>
2015-03-13 21:55:55 +0300

    Test 1st commit

---
+++ Hilyuta.Gallery.Model/Hilyuta.Gallery.Model/Picture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hilyuta.Gallery.Model
+{
+    public class Picture
+    {
+        public string name { set; get; }
+        public uint yearThePainting { set; get; }
+        public Artist author;
+
+    }
+}
